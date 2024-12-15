23e2a13eea70d4c9f3a01618da221d7f85fad2d2
Ankit <a.arya251001@gmail.com>
2024-12-15 23:09:56 +0530

    First working project setup with docker compose

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // Enables access from external devices
+    port: 5173, // Optional: Customize if needed
+    watch: {
+      usePolling: true, // Ensures file changes are detected in Docker
+    },
+  },
 })
